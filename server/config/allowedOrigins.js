ff5484b243b58f95e3dd256ff596dd3333a72820
mahimnamehta <mahimna.mehta99@gmail.com>
2023-07-02 02:01:01 +0530

    added aproved oridin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
+  "https://appointment-booking-app-cqd1.onrender.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
